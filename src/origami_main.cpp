3bd38af5f7fd1a890264529867d1384f7aff4dd0
lucbettaieb <luc.bettaieb@gmail.com>
2016-04-07 01:57:39 -0400

    test no 0 more

--- src/origami_main.cpp
+++ src/origami_main.cpp
@@ -69,8 +69,8 @@ void OrigamiBot::twistCB(const geometry_msgs::Twist& twist)
 
   if (x > 0)  // Forward motors
   {
-    softPwmWrite(L_REV, 0);
-    softPwmWrite(R_REV, 0);
+    //softPwmWrite(L_REV, 0);
+    //softPwmWrite(R_REV, 0);
 
     softPwmWrite(L_FWD, 100 * cmd_left);
     softPwmWrite(R_FWD, 100 * cmd_right);
@@ -78,8 +78,8 @@ void OrigamiBot::twistCB(const geometry_msgs::Twist& twist)
   }
   else if (x < 0)  // Reverse motors
   {
-    softPwmWrite(L_FWD, 0);
-    softPwmWrite(R_FWD, 0);
+    //softPwmWrite(L_FWD, 0);
+    //softPwmWrite(R_FWD, 0);
 
     softPwmWrite(L_REV, 100 * cmd_left);
     softPwmWrite(R_REV, 100 * cmd_right);
