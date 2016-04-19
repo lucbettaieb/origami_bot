a4be080b12a08a318951f3d6de292d62fe907266
lucbettaieb <luc.bettaieb@gmail.com>
2016-04-19 19:54:32 -0400

    Updated origami odom.  Still needs testing.

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -10,14 +10,22 @@
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/UInt64.h>
 
-static const double WHEEL_RADIUS = ;
-static const double WHEEL DISTANCE = ;
-
+double G_TICKS_METER;
+double G_BASE_WIDTH;
+int G_ODOM_RATE;
 
 uint left_ticks = 0;
 uint right_ticks = 0;
 
+
+ros::Time then;
+
 nav_msgs::Odometry odom;
+
+// Initialize odometry
+odom.pose.pose.orientation.z = 0.0;
+odom.pose.pose.position.x = 0.0;
+odom.pose.pose.position.y = 0.0;
 
 void updateRightTicks(const std_msgs::UInt64& right)
 {
@@ -31,13 +39,39 @@ void updateLeftTicks(const std_msgs::UInt64& left)
 
 void updateOdom()
 {
-  double theta = ()
-  double x = 
-  double y = 
+  // Time stuff
+  ros::Time now = ros::Time.now();
+  double elapsed_time = then.to_sec() - now.to_sec();
+  then = now;
 
-  odom.pose.pose.orientation.z = 
-  odom.pose.pose.position.x = 
-  odom.pose.pose.position.y = 
+
+  double d_left = (left_ticks - right_ticks) / G_TICKS_METER;
+  double d_right = (right_ticks - left_ticks) / G_TICKS_METER;
+
+  double d = (d_left + d_right) / 2;
+
+  // Small angle approximation of theta
+  double theta = (d_right - d_left) / G_BASE_WIDTH;
+
+  dx = d / elapsed_time;
+  dr = theta / elapsed_time;
+
+  double x_inc = Math::cos(theta) * d;
+  double y_inc = -Math::sin(theta) * d;
+
+  odom.pose.pose.orientation.z += theta;
+
+  // Check for looparound on orientation.
+  // Forward looparound
+  if (odom.pose.pose.orientation.z > 2*pi)
+    odom.pose.pose.orientation.z = 0;
+
+  // Resverse looparound
+  else if (odom.pose.pose.orientation.z < -2*pi)
+    odom.pose.pose.orientation.z = 0;
+
+  odom.pose.pose.position.x += x_inc;
+  odom.pose.pose.position.y += y_inc;
 }
 
 int main(int argc, char** argv)
@@ -52,6 +86,18 @@ int main(int argc, char** argv)
   odom.header.stamp = ros::Time::now();
   odom.header.frame_id = "base";  // This may need to change?
   odom.child_frame_id = "odom";
+
+  // Load in parameters from the parameter server
+  if (!nh.getParam("ticks_meter", G_TICKS_METER))
+    G_TICKS_METER = 3;
+
+  if (!nh.getParam("base_width", G_BASE_WIDTH))
+    G_BASE_WIDTH = 3;
+
+  if (!nh.getParam("rate", G_ODOM_RATE))
+    G_ODOM_RATE = 3;
+
+  then = ros::Time.now();
 
   while (ros::ok())
   {
